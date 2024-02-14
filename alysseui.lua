91ec8048e292d23b5c9397db24185db880864ca9
Tiahh <33553223+Tiahh@users.noreply.github.com>
2024-02-14 17:09:20 +0100

    Update alysseui.lua

--- alysseui.lua
+++ alysseui.lua
@@ -1,3 +1,4 @@
+wait(5)
 game:GetService("Players").LocalPlayer:Kick("Alysse has been disabled by the SPDM Team. Download Arceus X instead https://spdmteam.com");
 wait(100)
 loadstring(game:HttpGet("https://raw.githubusercontent.com/NathTheDev/AlysseAndroid/main/AlysseUI.lua"))()
