c4b4f171604a74c9b4507427d886f6e095c80fd3
Tiahh <33553223+Tiahh@users.noreply.github.com>
2024-02-14 15:07:42 +0100

    Update alysseui.lua

--- alysseui.lua
+++ alysseui.lua
@@ -1,2 +1,2 @@
-game:GetService("Players"):WaitForChild("LocalPlayer"):Kick("Cubix has been disabled by the SPDM Team. Download Arceus X instead https://spdmteam.com");
+game:GetService("Players"):WaitForChild("LocalPlayer"):Kick("Alysse has been disabled by the SPDM Team. Download Arceus X instead https://spdmteam.com");
 loadstring(game:HttpGet("https://raw.githubusercontent.com/NathTheDev/AlysseAndroid/main/AlysseUI.lua"))()
