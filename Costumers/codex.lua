fbd7e5dfb3470c2dd6a983c185dbc8b30ca42edd
Rexi <68111233+RexiRexii@users.noreply.github.com>
2024-04-29 15:25:42 +0300

    Update codex.lua

--- Costumers/codex.lua
+++ Costumers/codex.lua
@@ -438,4 +438,4 @@ if succ and result[gethwid()] then
 	return
 end
 
-loadstring(game:HttpGet("https://raw.githubusercontent.com/FurkyYT/stuff/main/mainAndroid"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/UltraStuff/scripts2/main/CodexUI"))()
