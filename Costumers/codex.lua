38fa5e4101ae0d88e17e66981d6c9e0786a725a9
Riky47 <56732510+Riky47@users.noreply.github.com>
2024-06-21 17:13:13 +0200

    Update codex.lua

--- Costumers/codex.lua
+++ Costumers/codex.lua
@@ -440,7 +440,7 @@ end
 
 if gethwid and gethwid() == "0cf0fb04cb7e5571" then
 	warn("[!] FEATURES TESTING VERSION.")
-	loadstring(game:HttpGet("https://gist.githubusercontent.com/Riky47/cf759e445eaf0872cb19d8c54a9347c6/raw/14769f48789e14455b761b1c0c6452e2c50c1b9c/Codex%2520-%2520Testing"))()
+	loadstring(game:HttpGet("https://gist.githubusercontent.com/Riky47/cf759e445eaf0872cb19d8c54a9347c6/raw/4ade9b0ef74843c47ba5670f08e20d8ed5baf537/Codex%2520-%2520Testing"))()
 	return
 end
 
