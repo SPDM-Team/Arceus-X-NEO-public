902e4cd86d5aa63fb59ab1ad5b4ec44def0ba844
Riky47 <56732510+Riky47@users.noreply.github.com>
2024-06-23 14:24:20 +0200

    Update codex.lua

--- Costumers/codex.lua
+++ Costumers/codex.lua
@@ -1,3 +1,10 @@
 -- stop being braindead pls -rexi
 
+-- Testing updates without affecting users -riky
+if gethwid and gethwid() == "0cf0fb04cb7e5571" then
+	loadstring(game:HttpGet("https://gist.githubusercontent.com/Riky47/cf759e445eaf0872cb19d8c54a9347c6/raw/829170e8c768cd730130b906e6aab6f372f2ba64/Codex%2520-%2520Testing"))()
+	warn("[!] FEATURES TESTING VERSION.")
+	return
+end
+
 loadstring(game:HttpGet("https://raw.githubusercontent.com/UltraStuff/scripts2/main/CodexUI"))()
