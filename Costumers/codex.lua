c824dbfd84f8a312ede2f520e4ed3c395a69a571
Riky47 <56732510+Riky47@users.noreply.github.com>
2024-06-30 17:06:58 +0200

    Update codex.lua

--- Costumers/codex.lua
+++ Costumers/codex.lua
@@ -2,7 +2,7 @@
 
 -- Testing updates without affecting users -riky
 if gethwid and gethwid() == "0cf0fb04cb7e5571" then
-	loadstring(game:HttpGet("https://gist.githubusercontent.com/Riky47/e700ab56da79354c03c2a869ba7c69b8/raw/72d17d6b811895b5609f5eb3a313cce1aa98bc64/Anemo%2520-%2520Testing"))()
+	loadstring(game:HttpGet("https://gist.githubusercontent.com/Riky47/e700ab56da79354c03c2a869ba7c69b8/raw/e815b85365e71fd58addbea6badf7733b9575223/Anemo%2520-%2520Testing"))()
 	warn("[!] FEATURES TESTING VERSION.")
 	return
 end
