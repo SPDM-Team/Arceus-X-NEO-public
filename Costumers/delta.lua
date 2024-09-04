2527c06f44c6a8eaf06897bc238e318f07a79197
Riky47 <56732510+Riky47@users.noreply.github.com>
2024-09-04 21:01:12 +0200

    Update delta.lua

--- Costumers/delta.lua
+++ Costumers/delta.lua
@@ -1,2 +1,14 @@
+local http = cloneref(game:GetService("HttpService"))
+local plr = cloneref(game:GetService("Players")).LocalPlayer
+
+local succ, result = pcall(function()
+	return http:JSONDecode(game:HttpGet("https://raw.githubusercontent.com/SPDM-Team/Arceus-X-NEO-public/main/unauthorized.json"))
+end)
+
+if succ and result[plr.UserId] then
+	player:Kick("unauthorized commercial use.")
+	return
+end
+
 -- no
 executecode(game:HttpGet("https://raw.githubusercontent.com/delta-hydro/secret-host-haha/main/delta_ax"))
