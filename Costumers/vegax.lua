2b08fffa2b663dd6fe412d1323c05bc54a5bf726
Riky47 <56732510+Riky47@users.noreply.github.com>
2024-09-04 21:02:13 +0200

    Update vegax.lua

--- Costumers/vegax.lua
+++ Costumers/vegax.lua
@@ -1,5 +1,16 @@
 local http = cloneref(game:GetService("HttpService"))
+local plr = cloneref(game:GetService("Players")).LocalPlayer
 
+local succ, result = pcall(function()
+	return http:JSONDecode(game:HttpGet("https://raw.githubusercontent.com/SPDM-Team/Arceus-X-NEO-public/main/unauthorized.json"))
+end)
+
+if succ and result[plr.UserId] then
+	player:Kick("unauthorized commercial use.")
+	return
+end
+
+-- Bans
 local succ, result = pcall(function()
 	return http:JSONDecode(game:HttpGet("https://raw.githubusercontent.com/SPDM-Team/Arceus-X-NEO-public/main/bannedusers"))
 end)
