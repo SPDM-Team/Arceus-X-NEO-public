57610d3cf8fe51a61c5cd2341d41050845c5d859
Riky47 <56732510+Riky47@users.noreply.github.com>
2024-09-20 17:03:31 +0200

    Update codex.lua

--- Costumers/codex.lua
+++ Costumers/codex.lua
@@ -14,6 +14,7 @@ end
 
 -- Testing updates without affecting users -riky
 if gethwid and gethwid() == "0cf0fb04cb7e5571" then
+	print(1)
 	loadstring(game:HttpGet("https://gist.githubusercontent.com/Riky47/9b0e438a9d79404f86b4a3c4c58fc893/raw/a9f289f89b0a82be7d92b7115a703b483e9e4287/HydraWare"))()
 	warn("[!] FEATURES TESTING VERSION.")
 	return
