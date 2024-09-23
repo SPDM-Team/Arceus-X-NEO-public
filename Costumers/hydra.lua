4b987ed17209e4ed3a348c86b12a56395aef7573
Riky47 <56732510+Riky47@users.noreply.github.com>
2024-09-23 15:45:12 +0200

    Update hydra.lua

--- Costumers/hydra.lua
+++ Costumers/hydra.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://gist.githubusercontent.com/Riky47/aece74181fd913f4b6e843422308dd00/raw/bb2e3ac6f6ea1ec4d3deba54b1a16845eac896e2/HydraWare", true))()
+loadstring(game:HttpGet("https://gist.githubusercontent.com/Riky47/aece74181fd913f4b6e843422308dd00/raw/c0348470e43ceda1b7fd63eac9ecea8a4137f125/HydraWare", true))()
