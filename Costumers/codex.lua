cb720cd404b305055d23205599fd640eda4c39fd
Tiahh <33553223+Tiahh@users.noreply.github.com>
2024-12-04 17:16:51 +0100

    Update codex.lua

--- Costumers/codex.lua
+++ Costumers/codex.lua
@@ -1,17 +1,3 @@
--- stop being braindead pls -rexi
-
-local http = cloneref(game:GetService("HttpService"))
-local plr = cloneref(game:GetService("Players")).LocalPlayer
-
-local succ, result = pcall(function()
-	return http:JSONDecode(game:HttpGet("https://raw.githubusercontent.com/SPDM-Team/Arceus-X-NEO-public/main/unauthorized.json"))
-end)
-
-if succ and result[plr.UserId] then
-	player:Kick("unauthorized commercial use.")
-	return
-end
-
 -- Testing updates without affecting users -riky
 if gethwid and gethwid() == "0cf0fb04cb7e5571" then
 	warn("Version tester executing init file.")
