a920415a208f6762dfa3952bfc73935024e00f17
Tiahh <33553223+Tiahh@users.noreply.github.com>
2024-12-04 16:41:13 +0100

    Update trigon.lua

--- Costumers/trigon.lua
+++ Costumers/trigon.lua
@@ -1,13 +1,1 @@
-local http = cloneref(game:GetService("HttpService"))
-local plr = cloneref(game:GetService("Players")).LocalPlayer
-
-local succ, result = pcall(function()
-	return http:JSONDecode(game:HttpGet("https://raw.githubusercontent.com/SPDM-Team/Arceus-X-NEO-public/main/unauthorized.json"))
-end)
-
-if succ and result[plr.UserId] then
-	player:Kick("unauthorized commercial use.")
-	return
-end
-
 executecode(game:HttpGet("https://raw.githubusercontent.com/relbaldski/bald/main/baldscript.lua"))
