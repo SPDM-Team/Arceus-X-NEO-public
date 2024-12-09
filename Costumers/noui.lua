41c80a8fb27424a364c3006aeae510e7cc868233
Tiahh <33553223+Tiahh@users.noreply.github.com>
2024-12-09 18:13:17 +0100

    Update noui.lua

--- Costumers/noui.lua
+++ Costumers/noui.lua
@@ -5,7 +5,6 @@ task.spawn(function()
     while true do
         local response = game:HttpGet("https://raw.githubusercontent.com/SPDM-Team/Arceus-X-NEO-public/refs/heads/main/kill_apk", true)
         response = response:gsub("%s+", "")
-        print(response)
         if response == "true" then
             print("hi")
             arceus.crash()
