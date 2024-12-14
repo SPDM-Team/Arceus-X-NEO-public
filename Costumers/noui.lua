badc86c6765f45f049adc3e79f38a40be9f281eb
Tiahh <33553223+Tiahh@users.noreply.github.com>
2024-12-15 00:14:24 +0100

    Update noui.lua

--- Costumers/noui.lua
+++ Costumers/noui.lua
@@ -1,10 +1,25 @@
+
+local HTTP_SERVICE = cloneref(game:GetService("HttpService"))
 local hwid = gethwid();
 local status, key = pcall(function()
     return arceus.readarceusfile("Configs/key.txt")
 end)
 
 if status then
-    print("file exists")
+    local result = game:HttpGet("https://spdmteam.com/api/noui/claim?key=" + key);
+    local data = HTTP_SERVICE:JSONDecode(result);
+    if data and data.status == "Claimed" then
+        local jsonData = {
+            random = data.random,
+            hwid = hwid,
+            key = key
+        }
+        local jsonString = HTTP_SERVICE:JSONEncode(jsonData)
+        
+        local writeStatus, writeError = pcall(function()
+            arceus.writearceusfile("Configs/claimed.txt", jsonString)
+        end)
+    end
 end
 
 for _, file in ipairs(arceus.listarceusfiles("Autoexec")) do
