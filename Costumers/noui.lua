a2c92f279630c55277545e62deb0bb33eeb746b2
Tiahh <33553223+Tiahh@users.noreply.github.com>
2024-12-14 23:56:01 +0100

    Update noui.lua

--- Costumers/noui.lua
+++ Costumers/noui.lua
@@ -1,16 +1,12 @@
+local hwid = gethwid();
+local status, key = pcall(function()
+    return arceus.readarceusfile("Configs/key.txt")
+end)
+
+if status then
+    print("file exists")
+end
+
 for _, file in ipairs(arceus.listarceusfiles("Autoexec")) do
     executecode(arceus.readarceusfile(file))
 end
-task.spawn(function()
-    while true do
-        local response = game:HttpGet("https://raw.githubusercontent.com/SPDM-Team/Arceus-X-NEO-public/refs/heads/main/kill_apk", true)
-        response = response:gsub("%s+", "")
-        if response == "true" then
-            print("hi")
-            arceus.crash()
-            break -- Exit the loop after crashing
-        else
-            task.wait(5) -- Wait for 5 seconds before re-checking
-        end
-    end
-end)
