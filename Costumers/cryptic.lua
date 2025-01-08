49dac01466276cc43841c221f64a605c85ac4251
Riky47 <56732510+Riky47@users.noreply.github.com>
2025-01-08 23:45:10 +0100

    Update cryptic.lua

--- Costumers/cryptic.lua
+++ Costumers/cryptic.lua
@@ -1,1 +1,1 @@
-executecode(game:HttpGet("https://github.com/AyoReaper/Cryptic/blob/main/CrypticKeySystemUserInterface"))
+executecode(game:HttpGet("https://raw.githubusercontent.com/AyoReaper/Cryptic/refs/heads/main/CrypticKeySystemUserInterface"))
