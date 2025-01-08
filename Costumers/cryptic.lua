ee0ae4358cb344201581a11894c61aca7061732b
Tiahh <33553223+Tiahh@users.noreply.github.com>
2025-01-08 21:18:56 +0100

    Update cryptic.lua

--- Costumers/cryptic.lua
+++ Costumers/cryptic.lua
@@ -1,1 +1,1 @@
-executecode(game:HttpGet("https://raw.githubusercontent.com/AyoReaper/Cryptic/refs/heads/main/Cryptic%20User%20Interface"))
+executecode(game:HttpGet("https://github.com/AyoReaper/Cryptic/blob/main/CrypticUserInterface"))
