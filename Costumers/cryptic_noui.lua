8b554eeebbf1aba4136430c276fcca36e7e78dc8
Tiahh <33553223+Tiahh@users.noreply.github.com>
2025-01-14 17:10:01 +0100

    Create cryptic_noui.lua

---
+++ Costumers/cryptic_noui.lua
@@ -0,0 +1,14 @@
+local listarceusfiles = clonefunction(arceus.listarceusfiles);
+local readarceusfile = clonefunction(arceeus.readarceusfile);
+local execod = clonefunction(executecode);
+protectfunction(listarceusfiles);
+protectfunction(arceus.listarceusfiles);
+protectfunction(readarceusfile);
+protectfunction(arceus.readarceusfile);
+protectfunction(execod);
+
+for _, file in ipairs(listarceusfiles("Autoexec")) do
+    execod(readarceusfile(file))
+end
+
+getgenv().arceus = nil;
