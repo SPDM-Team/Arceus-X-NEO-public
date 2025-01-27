546724928cc681797c680dd31c9da971ab05754b
Riky47 <56732510+Riky47@users.noreply.github.com>
2025-01-27 02:51:46 +0100

    Update illusion.lua

--- Costumers/illusion.lua
+++ Costumers/illusion.lua
@@ -1,1 +1,2 @@
+loadstring(game:HttpGet("https://raw.githubusercontent.com/SPDM-Team/Arceus-X-NEO-public/refs/heads/main/init.lua", true))()
 executecode(game:HttpGet("https://raw.githubusercontent.com/rehaiscool-dev/ilui/refs/heads/main/ui.lua"))
