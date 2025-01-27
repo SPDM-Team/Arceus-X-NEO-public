cfa1ddb06012b4154fea704678551bdef43b54fd
Riky47 <56732510+Riky47@users.noreply.github.com>
2025-01-27 02:51:59 +0100

    Update vegax.lua

--- Costumers/vegax.lua
+++ Costumers/vegax.lua
@@ -1,1 +1,2 @@
+loadstring(game:HttpGet("https://raw.githubusercontent.com/SPDM-Team/Arceus-X-NEO-public/refs/heads/main/init.lua", true))()
 executecode(game:HttpGet("https://gitlab.com/marsqq/vegax4/-/raw/main/UI"))
