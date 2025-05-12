677773b19c9b4f3a639becae270b01dc129850c9
Riky47 <56732510+Riky47@users.noreply.github.com>
2025-05-12 10:41:00 +0200

    Update codex.lua

--- Costumers/codex.lua
+++ Costumers/codex.lua
@@ -6,6 +6,8 @@ local whitelist = {
     "94709c29191e6396", "41d9fd434a3716e7",
     "af15143601cdd261", "74f9c74111c1e6f2",
     "fd4e1f5c3b175b1d", "74f9c74111c1e6f2",
+    "02b4b02b35dd9cc4",
+	
     "397A62E6-297F-4706-88AA-7C00CA31FB6B", -- iOS
     "2574FBB5-C46B-4CC6-AF83-86185A05F63D"
 }
