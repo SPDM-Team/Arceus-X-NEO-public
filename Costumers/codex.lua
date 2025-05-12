e9138a3ad3ad7df1dcfd5cd7188d2af5a4ff449e
Riky47 <56732510+Riky47@users.noreply.github.com>
2025-05-12 10:35:53 +0200

    Update codex.lua

--- Costumers/codex.lua
+++ Costumers/codex.lua
@@ -1,11 +1,18 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/SPDM-Team/Arceus-X-NEO-public/refs/heads/main/init.lua", true))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/SPDM-Team/Arceus-X-NEO-public/refs/heads/main/init.lua"))()
 
--- Testing updates without affecting users -riky
-if gethwid and gethwid() == "0cf0fb04cb7e5571" then
-	warn("Version tester executing init file.")
-	executecode(readfile("Init.lua"))
-	warn("[!] FEATURES TESTING VERSION.")
-	return
+-- Beta testing
+local whitelist = {
+    "0cf0fb04cb7e5571", "30baba33dcefc57c", -- Android
+    "94709c29191e6396", "41d9fd434a3716e7",
+    "af15143601cdd261", "74f9c74111c1e6f2",
+    "fd4e1f5c3b175b1d", "74f9c74111c1e6f2",
+    "397A62E6-297F-4706-88AA-7C00CA31FB6B", -- iOS
+    "2574FBB5-C46B-4CC6-AF83-86185A05F63D"
+}
+
+if gethwid and table.find(whitelist, gethwid()) then
+	warn("[!] THIS IS A BETA TESTING VERSION OF CODEX")
+	return executecode(game:HttpGet("https://gist.githubusercontent.com/Riky47/1d3625ee40e98e70311370e8d6642172/raw/aec843e884cf73259ee59dd1b1dddf56b52542c0/Codex-Beta.lua"))
 end
 
-loadstring(game:HttpGet("https://raw.githubusercontent.com/UltraStuff/scripts2/main/CodexUI"))()
+executecode(game:HttpGet("https://raw.githubusercontent.com/UltraStuff/scripts2/main/CodexUI"))
