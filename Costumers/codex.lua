a5b4980eb0478c745f6e17471321c2e54c23f801
Riky47 <56732510+Riky47@users.noreply.github.com>
2025-08-20 18:22:28 +0200

    Update codex.lua

--- Costumers/codex.lua
+++ Costumers/codex.lua
@@ -5,7 +5,7 @@ local whitelist = {
     "0cf0fb04cb7e5571", "30baba33dcefc57c", -- Android
     "94709c29191e6396", "41d9fd434a3716e7",
     "af15143601cdd261", "fd4e1f5c3b175b1d",
-    "02b4b02b35dd9cc4",
+    "02b4b02b35dd9cc4", "4cd6f60cb195d6d9",
 	
     "397A62E6-297F-4706-88AA-7C00CA31FB6B", -- iOS
     "2574FBB5-C46B-4CC6-AF83-86185A05F63D",
