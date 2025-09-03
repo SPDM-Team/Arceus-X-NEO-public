c89613a6500b935bb9a49c4a91a4c514fe0be7af
Riky47 <56732510+Riky47@users.noreply.github.com>
2025-09-03 16:53:18 +0200

    Update codex.lua

--- Costumers/codex.lua
+++ Costumers/codex.lua
@@ -19,7 +19,7 @@ local succ, oldUI = pcall(isfile, "data/OldUI")
 if gethwid and table.find(whitelist, gethwid()) then
 	warn("[!] THIS IS A BETA TESTING VERSION OF CODEX")
 	if not succ or not oldUI then -- New UI
-		return executecode(game:HttpGet("https://gist.githubusercontent.com/Riky47/1d3625ee40e98e70311370e8d6642172/raw/335c738583b21cf95872b9e0bc5553dd450760a4/Codex-Beta.lua"))
+		return executecode(game:HttpGet("https://gist.githubusercontent.com/Riky47/1d3625ee40e98e70311370e8d6642172/raw/d7e04579b33fb54150d5018ac427334506452b59/Codex-Beta.lua"))
 	end
 end
 
