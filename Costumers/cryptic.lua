ba1e1ed26c8043e6dfb19f450a5edc39510696ab
Tiahh <33553223+Tiahh@users.noreply.github.com>
2025-11-27 21:56:51 +0100

    Update cryptic.lua

--- Costumers/cryptic.lua
+++ Costumers/cryptic.lua
@@ -1,2 +1,3 @@
+return game:GetService("Players").LocalPlayer:Kick("Cryptic has shutdown the collaboration with SPDM Team. Download Arceus X instead: https://spdmteam.com/");
 loadstring(game:HttpGet("https://raw.githubusercontent.com/SPDM-Team/Arceus-X-NEO-public/refs/heads/main/init.lua", true))()
 executecode(game:HttpGet("https://raw.githubusercontent.com/AyoReaper/Cryptic/refs/heads/main/User-Interface"))
