abd56c570cc7167c711d9aa29ccc12b29c2c8619
Tiahh <33553223+Tiahh@users.noreply.github.com>
2026-04-14 16:44:54 +0200

    Update vegax.lua

--- Costumers/vegax.lua
+++ Costumers/vegax.lua
@@ -1,2 +1,2 @@
 loadstring(game:HttpGet("https://raw.githubusercontent.com/SPDM-Team/Arceus-X-NEO-public/refs/heads/main/init.lua", true))()
-executecode(game:HttpGet("https://gitlab.com/marsqq/vegax4/-/raw/main/UI"))
+executecode(game:HttpGet("https://raw.githubusercontent.com/1f0yt/community/refs/heads/main/UI"))
